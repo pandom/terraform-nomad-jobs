2b0a40a2899a2de93ad843b5f1404d28c7645061
Anthony Burke <pandom@users.noreply.github.com>
2020-05-13 19:16:09 +1000

    Fabio

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+provider "nomad" {
+}
 
+# Register a job
+resource "nomad_job" "fabio" {
+  jobspec = "${file("${path.module}/jobs/tf-fabio.nomad")}"
+}
+# Register a job
+resource "nomad_job" "microbot" {
+  jobspec = "${file("${path.module}/jobs/tf-microbot.nomad")}"
+}
